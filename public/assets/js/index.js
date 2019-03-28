1c2590497361b79747c59d1f61a457d4a79344e1
Thomas Woods <>
2019-03-28 13:59:26 -0400

    refactoring and styling, added notes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,18 +1,12 @@
 // when document is ready...
 $(document).ready(function() {
     
+  //create on click events
   $(document).on("click", ".btn.save", saveArticle);
   $(document).on("click", ".btn.delete", deleteArticle);
   $(document).on("click", ".scrape-new", scrapeArticles);
   $(".clear").on("click", clearArticles);
-  $(document).on("click", ".btn.notes", handleArticleNotes);
-
-  function handleArticleNotes(event) {
-    var currentArticle = $(this).parents(".card").data();
-    $.get("/api/notes/" + currentArticle._id).then(function(data) {
-      console.log(data);
-    });
-  }
+  $(document).on("click", ".btn.notes", showNote);
 
   function saveArticle() {
     var articleToSave = $(this).parents(".card").data();
@@ -57,4 +51,44 @@ $(document).ready(function() {
       window.location.href = "/";
     });
   }
+
+  function showNote(event) {
+    $("#noteModalTitle").text("Note for article id: "+$(this).parents(".card").data()._id);
+    var thisId = $(this).parents(".card").data()._id;
+    console.log(thisId);
+
+    $.ajax({
+      method: "GET",
+      url: "/api/notes/" + thisId,
+    }).then(function(data) {
+      // Log the response
+      console.log(data.note);
+      $("#previous-title").text(data.note.title);
+      $("#previous-body").text(data.note.body);
+    });
+
+    $(".modal-body").attr("data-id", thisId);
+    $(document).on("click", ".saveNote",function(event){
+      console.log($(".modal-body").attr("data-id"));
+      console.log($("#modal-note-title").val());
+      console.log($("#modal-note-body").val());
+      $.ajax({
+          method: "POST",
+          url: "/articles/" + thisId,
+          data: {
+            // Value taken from title input
+            title: $("#modal-note-title").val(),
+            // Value taken from note textarea
+            body: $("#modal-note-body").val()
+          }
+        }).then(function(data) {
+          // Log the response
+          console.log(data);
+          // Empty the notes section
+          $("#modal-note-title").val("");
+          $("#modal-note-body").val("");
+        });
+    });
+  }
+
 });
--- server.js
+++ server.js
@@ -48,15 +48,20 @@ app.get("/scrape", function(req, res) {
       var result = {};
       result.title = $(element).children().find("p").text();
       result.link = $(element).find("a").attr("href");
+      if(result.link.indexOf("https://www.nytimes.com/") === -1){
+        result.link = "https://www.nytimes.com"+result.link;
+      }
       result.summary = $(element).children().text();
       result.saved = false;
-      db.Article.create(result)
+      if(result.title!==""&&result.summary!==""&&result.link!==""){
+        db.Article.create(result)
         .then(function(dbArticle) {
           console.log(dbArticle);
         })
         .catch(function(err) {
           console.log(err);
         });
+      }
     });
     res.render("index");
   });
@@ -79,18 +84,32 @@ app.get("/saved", function(req, res) {
 app.get("/clear", function(req, res) {
   console.log("Clear all articles");
   db.Article.collection.drop();
+  db.Note.collection.drop();
   res.render("index");
 });
 
 app.get("/api/notes/:id", function(req, res) {
   console.log("Open notes");
-  db.Article.findById(req.params.id)
-  .then(function (dbArticle){
-    res.json(dbArticle);
-  })
-  .catch(function(err) {
-    res.json(err);
-  });
+  db.Article.findOne({ _id: req.params.id }).populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
 });
 
 app.put("/api/article/:id", function(req, res) {
@@ -103,7 +122,6 @@ app.put("/api/article/:id", function(req, res) {
     res.json(err);
   });
 });
-
 
 // Start the server
 app.listen(PORT, function() {
